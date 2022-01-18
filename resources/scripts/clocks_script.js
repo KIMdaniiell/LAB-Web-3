d5254103e855945841e9de450190de3670bd6b88
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2022-01-18 22:12:07 +0300

    Add files via upload

--- resources/scripts/clocks_script.js
+++ resources/scripts/clocks_script.js
@@ -10,40 +10,9 @@ window.onload = function (){
     let month = document.getElementById("month").innerText;
     let year = document.getElementById("year").innerText;
 
-    console.log(hours + " ч " +minutes + " м " + seconds + " с ");
-    console.log(day + " д " +month + " м " + year + " г ");
-
-    //setInterval(refresh,1000 );
-    //setInterval(time, 8000);
-    setInterval(reload,1000);
+    setInterval(reload,7000);
 }
 
 function reload() {
     window.reload();
-}
-
-function refresh() {
-    let seconds = document.getElementById("seconds");
-    let intSeconds = parseInt(seconds.innerText);
-    if (intSeconds<59) {
-        intSeconds++;
-        seconds.innerText = intSeconds+"";
-    } else {
-        intSeconds=0;
-        let minutes = document.getElementById("minutes");
-        let intMinutes = parseInt(minutes.innerText);
-        if (intMinutes<59) {
-            intMinutes++;
-            minutes.innerText = intMinutes+"";
-        } else {
-            intMinutes=0;
-            let hours = document.getElementById("hours");
-            let intHours = parseInt(hours.innerText);
-            intHours++;
-            hours.innerText = intHours+"";
-        }
-    }
-}
-function time() {
-    $("#clocks").load(location.href + " #clocks");
 }
